1b31794bb3db42df0f3d2a71c916ae22f813cc70
FaabsD <fabian.et.je@hotmail.com>
2021-12-07 12:11:47 +0100

    changed browserSync config to actually reload the browser on file changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
 let mix = require('laravel-mix');
 
 mix.js('js/script.js', 'dist/js')
-    .browserSync('threejs.test');
+    .browserSync({
+        proxy: 'threejs.test',
+        files: [
+            'dist/js/script.js',
+            'index.html',
+        ]
+    });
